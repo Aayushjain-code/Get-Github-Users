1a35c61303cd10e4ad6ee5a8edde1e0c479ee774
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-08-18 16:42:25 +0530

    fusion chart -iniated

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -2,9 +2,41 @@ import React from 'react';
 import styled from 'styled-components';
 import { GithubContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+
+
+
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+
+  const chartData = [
+    {
+      label: "html",
+      value: "200"
+    },
+    {
+      label: "css",
+      value: "260"
+    },
+    {
+      label: "javascript",
+      value: "180"
+    },
+
+  ];
+
+
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
+  );
 };
+
+
+
+
 
 const Wrapper = styled.div`
   display: grid;
