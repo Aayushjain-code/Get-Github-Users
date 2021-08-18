ae004cf636ca973b82080d8fdf33c03d41af207b
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-08-18 08:10:20 +0530

    initial routes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { Info, Repos, User, Search, Navbar } from '../components';
 import loadingImage from '../images/preloader.gif';
 import { GithubContext } from '../context/context';
+
+
+
 const Dashboard = () => {
   return (
     <main>
-      <h2>Dashboard Page</h2>
+      <Navbar />
+      <Search />
+      <Info></Info>
+      <User />
+      <Repos />
     </main>
   );
 };
+
+
+
 
 export default Dashboard;
