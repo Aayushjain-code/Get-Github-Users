46c9a057aa158e1087e9633003fa612fa2ac577c
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-08-19 17:16:17 +0530

    setting up search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,9 +2,43 @@ import React from 'react';
 import styled from 'styled-components';
 import { MdSearch } from 'react-icons/md';
 import { GithubContext } from '../context/context';
+
+
+
+
 const Search = () => {
-  return <h2>search component</h2>;
+  const [user, setUser] = React.useState('');
+  //get thinghd from global context
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (user) {
+      //more logic coming up soon
+      //optional
+      //setUser('');
+    }
+    console.log(user);
+  };
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <form onSubmit={handleSubmit}>
+          <div className="form-control">
+            <MdSearch />
+            <input type='text' placeholder='enter github user'
+              value={user} onChange={(e) => setUser(e.target.value)} />
+            <button type="submit"> Search</button>
+          </div>
+        </form>
+        <h3>Request:60/60</h3>
+      </Wrapper>
+    </section>
+  );
 };
+
+
+
+
+
 
 const Wrapper = styled.div`
   position: relative;
