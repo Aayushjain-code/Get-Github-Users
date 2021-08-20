1cfaf6373eea9dfa19695806690723bcadfe1b8a
Aayush Jain <75216351+Aayushjain-code@users.noreply.github.com>
2021-08-20 17:49:24 +0530

    authentitation part done

--- src/index.js
+++ src/index.js
@@ -17,13 +17,12 @@ ReactDOM.render(
       domain="dev-f1g5bjqm.us.auth0.com"
       clientId="UqwY6dcfV5NkqYZsJy0oyt4X18z34iVy"
       redirectUri={window.location.origin}
+      cacheLocation="localstorage"
     >
       <GithubProvider>
         <App />
       </GithubProvider>
     </Auth0Provider>
-
-
   </React.StrictMode>,
   document.getElementById('root')
 );
